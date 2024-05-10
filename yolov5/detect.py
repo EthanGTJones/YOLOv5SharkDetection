db7e3de8d4f3bb91c1bd46cac1c2fbc52b3b16b1
Ethan Jones <116948347+EthanGTJones@users.noreply.github.com>
2024-05-10 12:35:36 +1000

    Help

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -70,7 +70,7 @@ from utils.torch_utils import select_device, smart_inference_mode
 def run(
     weights=ROOT / "yolov5s.pt",  # model path or triton URL
     source=ROOT / "data/images",  # file/dir/URL/glob/screen/0(webcam)
-    data=ROOT / "data/coco128.yaml",  # dataset.yaml path
+    data=ROOT / "data/data.yaml",  # dataset.yaml path
     imgsz=(640, 640),  # inference size (height, width)
     conf_thres=0.25,  # confidence threshold
     iou_thres=0.45,  # NMS IOU threshold
